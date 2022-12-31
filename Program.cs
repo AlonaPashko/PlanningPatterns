369755cf18b6b181d0b079a5edbd9bbf7f469b2f
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2022-12-31 17:31:43 +0100

    Added an example of Singleton pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿
+﻿using PlanningPatterns.DesignPatterns;
+using PlanningPatterns.DesignPatterns.CreationalPatterns.Singleton;
+
+Singleton firstInstance = Singleton.Instance();
+Singleton secondInstance = Singleton.Instance();
+
+if (firstInstance == secondInstance)
+{
+    Console.WriteLine("Great, we are same instances");
+}
+else
+{
+    Console.WriteLine("Singleton failed me");
+}
 
