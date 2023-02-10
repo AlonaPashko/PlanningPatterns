dec75dbaaac21093d2ec14fade3fb45030bc0fff
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2023-02-10 09:42:58 +0100

    added a simple example for KnownTypeAttribute and DataContract (serialization)

---
+++ Serialization/DataContractExample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningPatterns.Serialization
+{
+    internal class DataContractExample
+    {
+        [DataContract]
+
+        class Person
+        {
+            [DataMember]
+            string LastName { get; set; }
+            [DataMember]
+            string FirstName { get; set; }
+
+            public Person(string firstNameValue, string lastNameValue)
+            {
+                FirstName = firstNameValue;
+                LastName = lastNameValue;
+            }
+        }
+        [DataContract]
+        
+        class Person2 
+        {
+            [DataMember]
+            string lastName;
+
+            [DataMember]
+            string firstName;
+
+            string fullName;
+            public string FullName { get => fullName; }
+
+            //this ctor isn't call during deserealization
+            public Person2(string lastName, string firstName)
+            {
+                this.lastName = lastName;
+                this.firstName = firstName;
+                fullName = firstName + " " + lastName;
+            }
+            //this method is called after the object is completely deserealized.
+            //use it instead the ctor
+            [OnDeserialized]
+            void OnDeserialized(StreamingContext context)
+            {
+                fullName = firstName + " " + lastName;
+            }
+        }
+    }
+}
---
+++ Serialization/KnownTypeAttributeExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningPatterns.Serialization
+{
+    internal class KnownTypeAttributeExample
+    {
+        //the KnownTypeAttribute specifies types to be used during deserialization
+        [KnownType(typeof(USAddress))]
+        
+        [DataContract]
+        
+        class Person3
+        {
+            [DataMember]
+            string fullNameValue;
+            [DataMember]
+            Address address; //adress is abstract
+        }
+
+        [DataContract]
+        public class USAddress : Address
+        {
+            public override string FullAdress => throw new NotImplementedException();
+        }
+
+    }
+}
